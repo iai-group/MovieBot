da9507b150a971f2f612efd827519984fc6ac542
unknown <ivicaUiS@Bieber.presight.local>
2020-10-29 17:21:04 +0100

    quotation fixes

--- moviebot/nlu/annotators/slots.py
+++ moviebot/nlu/annotators/slots.py
@@ -7,22 +7,22 @@ from enum import Enum
 
 
 class Slots(Enum):
-    """This class contains the list of slots available in the database. This approach will help
-    the NLU to correctly identify slots"""
+    """This class contains the list of slots available in the database. This
+    approach will help the NLU to correctly identify slots"""
 
-    ID = "ID"
-    TITLE = "title"
-    GENRES = "genres"
-    KEYWORDS = "keywords"
-    DIRECTORS = "directors"
-    DURATION = "duration"
-    ACTORS = "actors"
-    PLOT = "plot"
-    YEAR = "year"
-    MOVIE_LINK = "imdb_link"
-    COVER_IMAGE = "cover_image"
-    RATING = "imdb_rating"
-    MORE_INFO = "more_info"
+    ID = 'ID'
+    TITLE = 'title'
+    GENRES = 'genres'
+    KEYWORDS = 'keywords'
+    DIRECTORS = 'directors'
+    DURATION = 'duration'
+    ACTORS = 'actors'
+    PLOT = 'plot'
+    YEAR = 'year'
+    MOVIE_LINK = 'imdb_link'
+    COVER_IMAGE = 'cover_image'
+    RATING = 'imdb_rating'
+    MORE_INFO = 'more_info'
 
     def __str__(self):
         return self.value
