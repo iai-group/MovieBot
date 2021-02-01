6129974274cc47fae1e80b2941d81fcc08fa1081
Sphumx <54357096+Sphumx@users.noreply.github.com>
2021-02-01 23:48:17 +0100

    Added some basic features

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ import random
 
 PAGE_ACCESS_TOKEN='EAA3t92dlcAsBAD7TTHtDRtNQEQ3tWXUGh3j1zyPQwdz4W5e1Vn2Rp8ZBmZBzm1jaQKPAs9dA6aLq1KfJ8FAdWvOCZAJQjxdyB4aUuXScr1qIX9hOyykzBK2TqV80znDcrW1QxHPScAmgMZBeIB2ZBG4ZByXcKZA5Kdsa16Ol0uF5AZDZD'
 GREETINGS = ['hey', 'sup','hi', 'hello']
-RPS = ['rock', 'paper', 'scissors']
-
-
+MOVIE_PROMTS = ['Which would you like?', 'Pick a movie!', 'Have a gander!']
+ACTION_MOVIES = ['The Dark Knight', 'The Lord of the Rings: The Return of the King', 'Avengers: Endgame']
+IMDB_LINKS = {'The Dark Knight':'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg',
+            'The Lord of the Rings: The Return of the King':'https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg',
+            'Avengers: Endgame':'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UX182_CR0,0,182,268_AL_.jpg'
+            }
+IMDB_INFO = { #Fetch from imdb, check legality on webscraping
+    'The Dark Knight': 'Title: a, Runtime: y, Rating: z\nSummary: x'
+}
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def receive_message():
@@ -21,7 +27,7 @@ def receive_message():
         return '400'
 
 
-    #POST, sent to bot
+    #POST, sent to bot, for now only reads msg sent from user, doesnt keep track of conversation i.e. can type "Action" to skip the "movie" prompt 
     else:
         data = json.loads(request.data)
         messaging_events = data['entry'][0]['messaging']
@@ -29,15 +35,109 @@ def receive_message():
 
         # Can be multiple messaging events, loops through all
         for msg in messaging_events:
+
             uid = msg['sender']['id']
+            print(f'MESSAGE CHECK NO BOT: {msg}')
             txtinput = msg['message'].get('text')
             responseMsg = 'PLACEHOLDER MSG'
-
-            if txtinput == 'challenge':
-                choice = RPS[random.randint(0,2)]
+            output = request.get_json()
+            print(output)
+            print('--------------------------')
+            
+            #How the bot will react
+             
+            if txtinput == 'movie' or txtinput == 'Not Interested':
+                choice = MOVIE_PROMTS[random.randint(0,2)]
                 responseMsg = choice
                 bot.send_quickbuttons(uid,responseMsg)
+            elif txtinput == 'Action':
+                movie = ACTION_MOVIES[0]
+                responseMsg = movie
+                img = IMDB_LINKS[movie]
+                bot.send_movieimg(uid, responseMsg, img)
+            elif txtinput == 'Fantasy':
+                movie = ACTION_MOVIES[1]
+                responseMsg = movie
+                img = IMDB_LINKS[movie]
+                bot.send_movieimg(uid, responseMsg, img)
+            elif txtinput == 'Sci-fi':
+                movie = ACTION_MOVIES[2]
+                responseMsg = movie
+                img = IMDB_LINKS[movie]
+                bot.send_movieimg(uid, responseMsg, img)
+                
+                
+            elif txtinput == 'Id like more!':
+                rfrom flask import Flask, request
+import json
+from bot import Bot
+import random
 
+PAGE_ACCESS_TOKEN='EAA3t92dlcAsBAD7TTHtDRtNQEQ3tWXUGh3j1zyPQwdz4W5e1Vn2Rp8ZBmZBzm1jaQKPAs9dA6aLq1KfJ8FAdWvOCZAJQjxdyB4aUuXScr1qIX9hOyykzBK2TqV80znDcrW1QxHPScAmgMZBeIB2ZBG4ZByXcKZA5Kdsa16Ol0uF5AZDZD'
+GREETINGS = ['hey', 'sup','hi', 'hello']
+MOVIE_PROMTS = ['Which would you like?', 'Pick a movie!', 'Have a gander!']
+ACTION_MOVIES = ['The Dark Knight', 'The Lord of the Rings: The Return of the King', 'Avengers: Endgame']
+IMDB_LINKS = {'The Dark Knight':'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg',
+            'The Lord of the Rings: The Return of the King':'https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg',
+            'Avengers: Endgame':'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UX182_CR0,0,182,268_AL_.jpg'
+            }
+IMDB_INFO = { #Fetch from imdb, check legality on webscraping
+    'The Dark Knight': 'Title: a, Runtime: y, Rating: z\nSummary: x'
+}
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def receive_message():
+    #GET
+    if request.method == 'GET':
+        #Token security check
+        VERIFY_TOKEN = request.args.get('hub.verify_token')
+        challenge = request.args.get('hub.challenge')
+        if VERIFY_TOKEN == 'RPSToken':
+            return str(challenge)
+        return '400'
+
+
+    #POST, sent to bot, for now only reads msg sent from user, doesnt keep track of conversation i.e. can type "Action" to skip the "movie" prompt 
+    else:
+        data = json.loads(request.data)
+        messaging_events = data['entry'][0]['messaging']
+        bot = Bot(PAGE_ACCESS_TOKEN)
+
+        # Can be multiple messaging events, loops through all
+        for msg in messaging_events:
+
+            uid = msg['sender']['id']
+            
+            txtinput = msg['message'].get('text')
+            responseMsg = 'PLACEHOLDER MSG'
+            output = request.get_json()
+            
+            #How the bot will react
+             
+            if txtinput == 'movie' or txtinput == 'Not Interested':
+                choice = MOVIE_PROMTS[random.randint(0,2)]
+                responseMsg = choice
+                bot.send_quickbuttons(uid,responseMsg)
+            elif txtinput == 'Action':
+                movie = ACTION_MOVIES[0]
+                responseMsg = movie
+                img = IMDB_LINKS[movie]
+                bot.send_movieimg(uid, responseMsg, img)
+            elif txtinput == 'Fantasy':
+                movie = ACTION_MOVIES[1]
+                responseMsg = movie
+                img = IMDB_LINKS[movie]
+                bot.send_movieimg(uid, responseMsg, img)
+            elif txtinput == 'Sci-fi':
+                movie = ACTION_MOVIES[2]
+                responseMsg = movie
+                img = IMDB_LINKS[movie]
+                bot.send_movieimg(uid, responseMsg, img)
+                
+                
+            elif txtinput == 'Id like more!':
+                responseMsg = IMDB_INFO['The Dark Knight']
+                bot.send_link(uid,responseMsg)                
             #Anything Else
             else:
                 #Greeting Check
@@ -47,7 +147,29 @@ def receive_message():
                         responseMsg = 'Hello! Testing specific word'
                 # TEST PRINTS
                 #print(f'UID: {uid} - {txtinput}')
-                bot.send_message(uid, responseMsg)
+
+    
+            bot.send_message(uid, responseMsg)
+
+        return  '200'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+esponseMsg = IMDB_INFO['The Dark Knight']
+                bot.send_link(uid,responseMsg)                
+            #Anything Else
+            else:
+                #Greeting Check
+                words = txtinput.split()
+                for word in words:
+                    if word in GREETINGS:
+                        responseMsg = 'Hello! Testing specific word'
+                # TEST PRINTS
+                #print(f'UID: {uid} - {txtinput}')
+
+    
+            bot.send_message(uid, responseMsg)
 
         return  '200'
 
--- bot.py
+++ bot.py
@@ -37,16 +37,16 @@ class Bot(object):
       'message':{'text':message, 'quick_replies':[
         {
           'content_type':'text',
-          'title':'Rock',
-          'payload':'Rock'
+          'title':'Action', #Displayed on button
+          'payload':'Action' #Sent back to bot
         },{
           'content_type':'text',
-          'title':'Paper',
-          'payload':'Paper'
+          'title':'Fantasy',
+          'payload':'Fantasy'
         },{
           'content_type':'text',
-          'title':'Scissors',
-          'payload':'Scissors'
+          'title':'Sci-fi',
+          'payload':'Sci-fi'
         }
       ]
                  }
@@ -57,3 +57,74 @@ class Bot(object):
 
     response = requests.post(self.api_url, headers = headers, params=params, data=json.dumps(data))
 
+  def send_movieimg(self, psid, message, img, messaging_type='RESPONSE'):
+      headers = {
+        'Content-Type': 'application/json'
+      }
+
+      data = {
+        'messaging_type':messaging_type,
+        'recipient':{'id':psid},
+        'message':{ 
+            'attachment':{
+            'type':'image',
+            'payload':{
+                'url':img
+            }
+        },
+         'quick_replies':[
+          {
+            'content_type':'text',
+            'title':'Id like more!', #Displayed on button
+            'payload':'AM0I', #Sent back to bot
+            'image_url':'https://www.pinclipart.com/picdir/middle/538-5389478_-mint-plain-green-pastel-circle-color-colour.png'
+          },{
+            'content_type':'text',
+            'title':'Not Interested',
+            'payload':'Not Interested'
+          },
+          {
+            'content_type':'text',
+            'title':'Stop',
+            'payload':'Stop',
+            'image_url':'https://lh3.googleusercontent.com/proxy/upsXiwUR9I01YLLfSgp8ZjRkb2ZY1sahIp8lERrxcnwn-M1YTGzNBa45kQd-aZBfCg0341jAE-mrHup2XgiZAKghyplv56k'
+          }
+        ]
+                  }
+      }
+
+      params = {'access_token': self.access_token}
+      self.api_url = self.api_url + 'messages'
+
+      response = requests.post(self.api_url, headers = headers, params=params, data=json.dumps(data))
+
+  def send_link(self, psid, message, url='', messaging_type='RESPONSE'):
+      headers = {
+        'Content-Type': 'application/json'
+      }
+      data = {
+        'messaging_type':messaging_type,
+        'recipient':{'id':psid},
+        "message":{
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"button",
+        "text":message,
+        "buttons":[
+          {
+            "type":"web_url",
+            "url":"https://www.imdb.com/title/tt0468569/?ref_=adv_li_tt",
+            "title":"To imbd",
+            "webview_height_ratio": "full"
+          }
+        ]
+      }
+    }
+  }
+  }
+
+      params = {'access_token': self.access_token}
+      self.api_url = self.api_url + 'messages'
+
+      response = requests.post(self.api_url, headers = headers, params=params, data=json.dumps(data))
