e1052ea5d1409367114d0a25cced4e9a53427028
AleksanderD <Aleksander.m.drzewiecki@gmail.com>
2022-05-23 10:01:08 +0200

    Add support for flask controller and black formatting

--- moviebot/controller/server.py
+++ moviebot/controller/server.py
@@ -1,28 +1,33 @@
 """This file contains the flask server."""
 
-from moviebot.controller.controller_messenger import ControllerMessenger
-from moviebot.controller.controller_telegram import ControllerTelegram
+# from moviebot.controller.controller_messenger import ControllerMessenger
+# from moviebot.controller.controller_telegram import ControllerTelegram
+from moviebot.controller.controller_flask import ControllerFlask
 from flask import Flask, request
 from os import environ
 import yaml
 import telegram
 
 app = Flask(__name__)
-controller_messenger = ControllerMessenger()
-controller_telegram = ControllerTelegram()
-URL = "https://636d944311f9.ngrok.io/" # Webhook url
+# controller_messenger = ControllerMessenger()
+# controller_telegram = ControllerTelegram()
+controller_flask = ControllerFlask()
+URL = "https://636d944311f9.ngrok.io/"  # Webhook url
+
 
 def bot_token():
     """Gets bot token from config file."""
-    path = 'config/bot_token.yaml'
-    with open(path, 'r') as file:
+    path = "config/bot_token.yaml"
+    with open(path, "r") as file:
         config = yaml.load(file, Loader=yaml.Loader)
-        BOT_TOKEN = config['BOT_TOKEN']
+        BOT_TOKEN = config["BOT_TOKEN"]
         return BOT_TOKEN
+
 
 telegram_token = bot_token()
 bot = telegram.Bot(token=telegram_token)
 
+'''
 @app.route('/{}'.format(telegram_token), methods=['POST'])
 def respond():
     """Receives to Telegram POST request"""
@@ -39,69 +44,82 @@ def respond():
         controller_telegram.exit(update, True)
     else:
         controller_telegram.continue_conv(update, True)
-    return 'ok'
+    return 'ok' '''
+
 
 def set_webhook():
-    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=telegram_token))
+    s = bot.setWebhook("{URL}{HOOK}".format(URL=URL, HOOK=telegram_token))
     if s:
         return "webhook ok"
     else:
         return "webhook failed"
+
 
 def run(config):
     """Runs execute_agent in ControllerMessenger and starts flask server.
 
     Args:
         config: agent settings
-        
+
     """
-    controller_telegram.execute_agent(config)
-    controller_messenger.execute_agent(config)
+    # controller_telegram.execute_agent(config)
+    # controller_messenger.execute_agent(config)
+    controller_flask.execute_agent(config)
     # Messenger verify token
     verify_token()
     set_webhook()
-    app.run(host='0.0.0.0', port=environ.get("PORT", 5000))
-    
+    app.run(host="0.0.0.0", port=environ.get("PORT", 5001))
+
+
 def verify_token():
     """Gets verify token from config file."""
-    path = 'config/bot_token.yaml'
-    with open(path, 'r') as file:
+    path = "config/bot_token.yaml"
+    with open(path, "r") as file:
         config = yaml.load(file, Loader=yaml.Loader)
-        VERIFY_TOKEN = config['MESSENGER_VERIFY_TOKEN']
+        VERIFY_TOKEN = config["MESSENGER_VERIFY_TOKEN"]
         return VERIFY_TOKEN
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def receive_message():
     """Receives messenger POST requests"""
-    if request.method == 'GET':
+    if request.method == "GET":
         token_sent = request.args.get("hub.verify_token")
         return verify_fb_token(token_sent)
-    else:  
+    else:
         output = request.get_json()
         print(output)
-        action(output)
+        response = action(output)
+        if response:
+            return response
         return "Message Processed"
+
 
 def verify_fb_token(token_sent):
     if token_sent == verify_token():
         return request.args.get("hub.challenge")
-    return 'Invalid verification token'
+    return "Invalid verification token"
+
 
 def action(output):
     """Gets user id and payload from output and runs get_message in the controller.
 
-    Args: 
+    Args:
         output: output from request
 
     """
-    event = output['entry'][0]['messaging'][0]
-    user_id = event['sender']['id']
-    controller_messenger.initialize(user_id)
+    event = output["entry"][0]["messaging"][0]
+    user_id = event["sender"]["id"]
+    controller_flask.initialize(user_id)
     payload = get_message(output)
     print(payload)
     if payload is not None:
-        if controller_messenger.run_method(user_id, payload):
-            controller_messenger.send_message(user_id, payload)
+        run_method_response = controller_flask.run_method(user_id, payload)
+        if run_method_response == True:
+            return controller_flask.send_message(user_id, payload)
+        elif run_method_response:
+            return run_method_response
+
 
 def get_message(output):
     """Gets payload from output.
@@ -112,13 +130,12 @@ def get_message(output):
         string with payload
 
     """
-    for event in output['entry']:
-        for message in event['messaging']:
-            if message.get('message'):
-                if message['message'].get('quick_reply'):
-                    return message['message']['quick_reply']['payload']
-                if message['message'].get('text'): 
-                    return message['message']['text']
-            if message.get('postback'):
-                return message['postback']['payload']
-
+    for event in output["entry"]:
+        for message in event["messaging"]:
+            if message.get("message"):
+                if message["message"].get("quick_reply"):
+                    return message["message"]["quick_reply"]["payload"]
+                if message["message"].get("text"):
+                    return message["message"]["text"]
+            if message.get("postback"):
+                return message["postback"]["payload"]
