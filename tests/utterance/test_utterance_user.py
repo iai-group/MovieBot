9e0ff57650bf7338a03ae9bfb219ccdfb20d1d01
Ivica Kostric <ivica_kostric@yahoo.com>
2022-12-07 08:54:11 +0100

    Add pre-commit hooks and github actions (#61)

--- tests/utterance/test_utterance_user.py
+++ tests/utterance/test_utterance_user.py
@@ -1,21 +1,23 @@
-from moviebot.core.shared.utterance.utterance_user import UserUtterance
-
 import pytest
 
+from moviebot.core.shared.utterance.utterance import UserUtterance
 
-@pytest.mark.parametrize('utterance, expected', [
-    ('Hello WoRld', ['hello', 'world']),
-    ('document.with, punctuation:   with?spaces\ttabs\nwith newlines\n\n\n',
-     ['document', 'punctuation', 'space', 'tab', 'newlines']),
-])
+
+@pytest.mark.parametrize(
+    "utterance, expected",
+    [
+        ("Hello WoRld", ["hello", "world"]),
+        (
+            (
+                "document.with, punctuation:   with?spaces\ttabs\nwith"
+                " newlines\n\n\n"
+            ),
+            ["document", "punctuation", "space", "tab", "newlines"],
+        ),
+    ],
+)
 def test_get_processed_utterance(utterance, expected):
-    # Setup
     uu = UserUtterance(utterance)
-
-    # Exercise
     result = uu.get_preprocessed_utterance()
 
-    # Results
     assert result == expected
-
-    # Cleanup - none
