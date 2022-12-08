1826de27c80f8ff5beba6c0c490c5ed6f44b6f0e
IKostric <ivica_kostric@yahoo.com>
2022-12-08 12:48:06 +0100

    Restart functionality is broken in cmd line version Fixes #56

--- moviebot/controller/controller.py
+++ moviebot/controller/controller.py
@@ -1,25 +1,39 @@
 """This file contains the Controller which controls the conversation between
 the agent and the user."""
 
-__author__ = "Javeria Habib"
-
 from abc import ABC, abstractmethod
+from typing import Any, Dict
+
+from moviebot.agent.agent import Agent
+from moviebot.core.utterance.utterance import UserUtterance
+
+RESTART = "/restart"
 
 
 class Controller(ABC):
-    """This is the main class that controls the other components of the
-    IAI MovieBot. The controller executes the conversational agent."""
-
-    def __init__(self):
-        """Initializes some basic structs for the Controller."""
-
-    @abstractmethod
-    def execute_agent(self, configuration):
-        """Runs the conversational agent and executes the dialogue by calling
-        the basic components of IAI MovieBot
+    def __init__(self, configuration: Dict[str, Any]):
+        """This is the main class that controls the other components of the
+        IAI MovieBot. The controller executes the conversational agent.
 
         Args:
             configuration: the settings for the agent
-
         """
-        pass
+        self.configuration = configuration
+
+    @abstractmethod
+    def execute_agent(self):
+        """Runs the conversational agent and executes the dialogue by calling
+        the basic components of IAI MovieBot.
+        """
+        raise NotImplementedError
+
+    def initialize_agent(self) -> Agent:
+        agent = Agent(self.configuration)
+        agent.initialize()
+        print(
+            "The components for the conversation are initialized successfully."
+        )
+        return agent
+
+    def restart(self, utterance: UserUtterance) -> bool:
+        return utterance.get_text() == RESTART
--- moviebot/controller/controller_terminal.py
+++ moviebot/controller/controller_terminal.py
@@ -1,21 +1,17 @@
 """This file contains the Controller class which controls the flow of the
-conversation while the user interacts with the agent using python console"""
+conversation while the user interacts with the agent using python console.
+"""
 
-__author__ = "Javeria Habib"
 
-from moviebot.agent.agent import Agent
 from moviebot.controller.controller import Controller
-from moviebot.core.shared.utterance.utterance import UserUtterance
+from moviebot.core.utterance.utterance import UserUtterance
 
 
 class ControllerTerminal(Controller):
     """This is the main class that controls the other components of the
     IAI MovieBot. The controller executes the conversational agent."""
 
-    def __init__(self):
-        """Initializes some basic structs for the Controller."""
-
-    def execute_agent(self, configuration):
+    def execute_agent(self) -> None:
         """Runs the conversational agent and executes the dialogue by calling
         the basic components of IAI MovieBot
 
@@ -23,21 +19,24 @@ class ControllerTerminal(Controller):
             configuration: the settings for the agent
 
         """
-        agent = Agent(configuration)
-        agent.initialize()
-        print(
-            "The components for the conversation are initialized successfully."
-        )
-        user_options = {}
-        agent_response, user_options = agent.start_dialogue()
-        print(f"AGENT: {agent_response}")
-        while not agent.terminated_dialogue():
+        agent = None
+        while True:
             utterance = input("User: ")
             user_utterance = UserUtterance({"text": utterance})
-            agent_response, user_options = agent.continue_dialogue(
-                user_utterance, user_options
-            )
+
+            if not agent or self.restart(user_utterance):
+                agent = self.initialize_agent()
+                agent_response, user_options = agent.start_dialogue()
+            else:
+                agent_response, user_options = agent.continue_dialogue(
+                    user_utterance, user_options
+                )
+
             print(f"AGENT: {agent_response}")
             if user_options:
                 print(list(user_options.values()))
+
+            if agent.terminated_dialogue():
+                break
+
         agent.end_dialogue()
