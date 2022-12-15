c130d54444073f4b48eb3fcdb01319c97dedcdb7
Weronika Lajewska <lajewska@ux.uis.no>
2022-12-15 15:25:57 +0100

    fixing utterance parsing + tests

--- tests/nlu/test_rule_based_annotator.py
+++ tests/nlu/test_rule_based_annotator.py
@@ -1,9 +1,14 @@
 """Test rule based annotator (RBAnnotator)."""
 
+from unittest.mock import patch
+
 import pytest
+from tests.mocks.mock_data_loader import MockDataLoader
+from tests.mocks.mock_ontology import MockOntology
 
 from moviebot.core.shared.utterance.utterance import UserUtterance
 from moviebot.nlu.annotation.rule_based_annotator import RBAnnotator
+from moviebot.nlu.user_intents_checker import UserIntentsChecker
 
 SLOT_VALUES = {
     "actors": {
@@ -16,6 +21,20 @@ SLOT_VALUES = {
         "Tom Hanks": "tom hank",
     },
 }
+
+@pytest.fixture()
+@patch("moviebot.ontology.ontology.Ontology", new=MockOntology)
+@patch("moviebot.nlu.user_intents_checker.DataLoader", new=MockDataLoader)
+def uic() -> UserIntentsChecker:
+    """Returns a user intent checker fixture with an ontology."""
+    config = {
+        "ontology": MockOntology(None).load_ontolgy(),
+        "database": "",
+        "slot_values_path": "",
+        "tag_words_slots_path": "",
+    }
+    uic = UserIntentsChecker(config)
+    return uic
 
 
 @pytest.mark.parametrize(
@@ -64,3 +83,17 @@ def test__person_name_annotator_empty(utterance: UserUtterance) -> None:
     annotator = RBAnnotator(None, None, SLOT_VALUES)
     result = annotator._person_name_annotator(utterance)
     assert result is None
+
+
+@pytest.mark.parametrize(
+    "raw_utterance, gram, ngram_size, expected",
+    [
+        ("i would like to watch something funny+romantic", "funny", 1, "funny"),
+        ("im interested in something about kung-fu", "kung fu", 2, "kung fu"),
+    ],
+)
+def test_find_in_raw_utterance(uic: UserIntentsChecker, raw_utterance: str, gram: str, ngram_size: int, expected: str):
+    annotator = RBAnnotator(uic._process_utterance, uic._lemmatize_value, SLOT_VALUES)
+    result = annotator.find_in_raw_utterance(raw_utterance, gram, ngram_size)
+
+    assert result == expected
