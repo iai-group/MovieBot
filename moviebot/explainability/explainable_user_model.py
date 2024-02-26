db46912b96fd0b2cc2175743c2999e3e43ccbee1
IKostric <ivica_kostric@yahoo.com>
2024-02-26 11:59:38 +0100

    Add ExplainableUserModel Class Fixes #222

--- moviebot/explainability/explainable_user_model.py
+++ moviebot/explainability/explainable_user_model.py
@@ -1,23 +1,43 @@
 """Abstract class for creating explainable user models."""
 
 from abc import ABC, abstractmethod
-from typing import Dict
+from typing import Dict, List
 
-from dialoguekit.core import AnnotatedUtterance
+from dialoguekit.core import AnnotatedUtterance, Annotation
+
+from moviebot.user_modeling.user_model import UserModel
 
 UserPreferences = Dict[str, Dict[str, float]]
 
 
 class ExplainableUserModel(ABC):
-    @abstractmethod
-    def generate_explanation(
-        self, user_preferences: UserPreferences
-    ) -> AnnotatedUtterance:
-        """Generates an explanation based on the provided input data.
+    def __init__(self, user_model: UserModel) -> None:
+        self._user_model = user_model
 
-        Args:
-            input_data: The input data for which an explanation is to be
-            generated.
+    def get_preferences_as_annotations(self) -> List[Annotation]:
+        """Returns the key-value pairs of the user model.
+
+        Returns:
+            Key-value pairs of the user model.
+        """
+        all_preferences = self._user_model.get_all_slot_preferences()
+        return [
+            Annotation(key, value) for key, value in all_preferences.items()
+        ]
+
+    def generate_explanation(self) -> AnnotatedUtterance:
+        """Generates an explanation based on the user model.
+
+        Returns:
+            A system utterance containing an explanation.
+        """
+        explanation = self._generate_explanation()
+        explanation.annotations = self.get_preferences_as_annotations()
+        return explanation
+
+    @abstractmethod
+    def _generate_explanation(self) -> AnnotatedUtterance:
+        """Generates an explanation based on the provided input data.
 
         Returns:
             A system utterance containing an explanation.
